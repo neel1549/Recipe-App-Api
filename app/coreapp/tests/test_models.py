295bf3bbd96a10b67d0170c1df7cef017b2ea377
Neel Kattumadam <neelkk@gmail.com>
2020-05-12 19:18:04 -0700

    usermodels

---
+++ app/coreapp/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email(self):
+        email='neelkk@gmail.com'
+        password='arc123'
+        user=get_user_model().objects.create_user(
+        email=email,
+        password=password
+
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertEqual(user.check_password(password),True)
+
+
+    def test_new_user_normalized(self):
+        email="test@GMAIL.COM"
+        user=get_user_model().objects.create_user(
+        email=email,password="test123"
+        )
+
+        self.assertEqual(email.lower(),user.email)
+
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'test123')
+
+
+    def test_create_new_superuser(self):
+        user=get_user_model().objects.create_superuser(
+        'neelkk@gmail.com',
+        'arc123'
+
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+        
