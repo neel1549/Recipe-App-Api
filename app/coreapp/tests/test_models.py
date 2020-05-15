b142fa2340b2186019e434785c371c2b77cb76e0
Neel Kattumadam <neelkk@gmail.com>
2020-05-15 12:03:28 -0700

    Recipes

--- app/coreapp/tests/test_models.py
+++ app/coreapp/tests/test_models.py
@@ -51,3 +51,16 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag),tag.name)
+
+    def test_ingredient_str(self):
+        ingredient=models.Ingredient.objects.create(
+        user=sample_user(),
+        name='Cucumber'
+        )
+        self.assertEqual(str(ingredient),ingredient.name)
+
+    def test_recipe_str(self):
+        recipe=models.Recipe.objects.create(
+            user=sample_user(),title='Steak',
+            time_minutes=5,price=5.00
+        )
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from coreapp.models import Tag
+from coreapp.models import Tag,Ingredient,Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -8,4 +8,22 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model=Tag
         fields=('id','name')
-        read_only_fields=('id')
+        read_only_fields=('id',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Ingredient
+        fields=('id','name')
+        read_only_fields=('id',)
+
+class RecipeSerializer(serializers.ModelSerializer):
+    ingredients=serializers.PrimaryKeyRelatedField(
+        many=True,queryset=Ingredient.objects.all
+    )
+
+    class Meta:
+        model=Recipe
+        fields=('id','title','ingredients','tags','price','time_minutes','link')
+        read_only_fields=('id',)
